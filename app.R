92dd03e80ca697878d76c656cd3eb6e819b4a70e
Diego Martinez <dfmartinezg@pm.me>
2019-06-09 00:12:53 -0500

    Primeros controles y lógica para importar archivos. Tabla temporal con fines de codificación

--- app.R
+++ app.R
@@ -9,49 +9,72 @@ library(httr)
 library(jsonlite)
 
 # Pruebas inciales conexion API MobilServ
-url_base <- "https://api.ucld.us/"
-url_final <- modify_url(url_base, path = "env/prd/authentication")
-clave <- POST(url_final, body = list(UserName = "diego.martinez@terpel.com", Password = "Tomate80"),
-              encode = "json",
-              add_headers(ContentType = "application/json",
-                          Accept = "*/*"
-                          ), verbose()) %>% 
-  content("text")
+# url_base <- "https://api.ucld.us/"
+# url_final <- modify_url(url_base, path = "env/prd/authentication")
+# clave <- POST(url_final, body = list(UserName = "diego.martinez@terpel.com", Password = "Tomate80"),
+#               encode = "json",
+#               add_headers(ContentType = "application/json",
+#                           Accept = "*/*"
+#                           ), verbose()) %>% 
+#   content("text")
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+# Interfaz de Usuario
+ui <- dashboardPage(
+  # Título de la Página
+  dashboardHeader(title = ("ANÁLISIS DE RESULTADOS MSLA")),
+  # Controles Comunes a toda la aplicación
+  dashboardSidebar(
+    fileInput("archivomsla", "Buscar Archivo descargado MSLA", multiple = FALSE,
+              accept = c(".xls", ".xlsx", ".csv"),
+              placeholder = "Ningún Archivo Seleccionado"),
+    dateRangeInput("fechas", "Rango de Fechas Activas", end = Sys.Date()),
+    selectizeInput("aplicacion", "Seleccione la Aplicación", choices = NULL)
+  ),
+  dashboardBody(
+    fluidRow(
+       box(which = "plot",
+        selectizeInput("prop1", "Seleccionar 1", choices = NULL),
+        selectizeInput("prop2", "Seleccionar 2", choices = NULL),
+        plotOutput("puntos")
+        ),
+       box(title = "Histograma",
+           plotOutput("hist"))
+          ),
+    fluidRow(
+      dataTableOutput("tabla")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  options(shiny.maxRequestSize=30*1024^2)
+  
+  datos <- reactive({
+    archivos <- input$archivomsla
+    if(is.null(archivos)){ 
+      return(NULL)
+    } else if(archivos$type == "text/csv"){
+      withProgress(message = "Importando Archivo",{
+        datos <- read.csv(archivos$datapath)
+      })
+    } else{
+      withProgress(message = "Importando Datos", {
+        datos <- readxl::read_excel(archivos$datapath)
+      })
+    }
+  })
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$tabla <- renderDataTable({
+    datos()
+  })
+  
+  output$puntos <- renderPlot({
+    
+  })
+  observe({
+    updateSelectizeInput(session, "aplicacion", choices = as_factor(datos()$`Asset Class`))
+  })
 }
 
 # Run the application 
