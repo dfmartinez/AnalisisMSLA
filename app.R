9c295cb18b73ca407226420fe8d9ebff194d1a07
Diego Francisco Martinez Galeano <diego.martinez@terpel.com>
2019-09-27 18:24:11 -0500

    Actualizaciones menores al código de la aplicación. Pruebas exitosas con número de asset

--- app.R
+++ app.R
@@ -15,18 +15,18 @@ options(shiny.reactlog = TRUE)
 # Interfaz de Usuario
 ui <- dashboardPage(
   # Título de la Página
-  dashboardHeader(title = ("ANÁLISIS DE RESULTADOS MSLA")),
+  dashboardHeader(title = ("ANÁLISIS DE RESULTADOS MSLA"), titleWidth = 250),
   # Controles Comunes a toda la aplicación
   dashboardSidebar(
     helpText("Ingrese Usuario y contraseña de MobilServ para iniciar"),
     textInput("user", "Usuario"),
     passwordInput("pass", "Contraseña"),
-    actionButton("login", "Iniciar")
-    # dateRangeInput("fechas", "Rango de Fechas Activas", end = Sys.Date()),
-    # selectizeInput("aplicacion", "Seleccione la Aplicación", choices = NULL)
+    actionButton("login", "Iniciar"),
+    br(),
+    uiOutput("filtros")
   ),
+  # Diseño de la página principal
   dashboardBody(
-    uiOutput("filtros"),
     verbatimTextOutput("txt1"),
     verbatimTextOutput("txt2")
     )
@@ -37,32 +37,81 @@ ui <- dashboardPage(
 server <- function(input, output, session) {
   
   # Función para interactuar con API
-  SolAPI <- function(path){
+  SLOAPI <- function(path, mode = c("get", "post"), token = NULL, param = NULL, body = NULL){
     url_base <- "https://api.ucld.us/"
-    url_mod <- modify_url(url_base, path = paste0("env/prd/",path))
+    url_mod <- modify_url(url_base, path = paste0("env/prd/",path), params = param)
+    # browser()
+    if(mode == "get"){
+      resp <- GET(url_mod,
+                  add_headers(Accept = "*/*",
+                              ContentType = "application/json",
+                              Authorization = token)) 
+    } else{
+      resp <- POST(url_mod,
+                   add_headers(Accept = "*/*",
+                               ContentType = "application/json",
+                               Authorization = token),
+                   body = body,
+                   encode = "json")
+    }
     
-    return(url_mod)
+    if(status_code(resp) != 200){
+      stop(
+        glue("Error en la conexión API\n", "Código Error {status_code(resp)}\n","Error: {message_for_status(resp)}"),
+        call. = FALSE
+      )
+    }
+    
+    resp %>% 
+      content(as = "text")
   }
   
+  cuentas <- reactiveVal(value = NULL, "cuenta")
+  
   observeEvent(input$login,{
-   clave <- POST(SolAPI("authentication"), 
-                 body = list(paste0(input$user,"@terpel.com"), input$pass),
-                 encode = "json",
-                 add_headers(Accept = "*/*",
-                            ContentType = "application/json")
-                 ) %>%
-     stop_for_status() %>%
-           content(as = "text") 
+   token <- SLOAPI("authentication", "post", body = list(UserName = paste0(input$user,"@terpel.com"), Password = input$pass))
+     
+     # POST(SolAPI("authentication"), 
+     #             body = list(UserName = paste0(input$user,"@terpel.com"), Password = input$pass),
+     #             encode = "json",
+     #             add_headers(Accept = "*/*",
+     #                        ContentType = "application/json")
+     #             ) %>%
+     # stop_for_status() %>%
+     #       content(as = "text") 
+   
+   cuentasmsla <- SLOAPI("account/getaccounts", "get", token = token) %>% 
+     fromJSON() %>%
+     filter(!row.names(.) %in% c("1","2")) %>%
+     split(.$name) %>%
+     map(~.$id)
+     
+     # GET(SolAPI("account/getaccounts"),
+     #              add_headers(Accept = "*/*",
+     #                          ContentType = "application/json",
+     #                          Authorization = token)) %>%
+     # stop_for_status() %>% 
+     # content(as = "text") %>% 
+     
+   
+   cuentas(cuentasmsla)
   })
   
+  # cuentas <- reactive({
+  #   GET(SolAPI("account/getaccounts"),
+  #       add_headers(Accept = "*/*",
+  #                   ContentType = "application/json",
+  #                   Authorization = token)) %>% 
+  #     stop_for_status() %>% 
+  #     content(as = "text") %>% 
+  #     fromJSON()
+  # })
+  
   output$filtros <- renderUI({
-    cuentas <- GET(SolAPI("account/getaccounts"),
-                   add_headers(Accept = "*/*",
-                               ContentType = "application/json", 
-                               Authorization = clave)) %>% 
-      stop_for_status()
+    if(input$login == 0)
+      return()
     
-    selectizeInput("cuentas", "Seleccionar las Cuentas", choices = cuentas)
+    selectizeInput("cuentas", "Seleccionar Cuenta", choices = cuentas(), options = list())
   })
   
 }
